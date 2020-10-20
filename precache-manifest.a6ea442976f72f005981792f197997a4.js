a785536d965dfa5de0d189196a6ad215aa480436
alza <alzadinaya@gmail.com>
2020-10-20 14:15:43 +0200

    Updates

---
+++ precache-manifest.a6ea442976f72f005981792f197997a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36fffaf1c20f9b0277d9beb5460b09ab",
+    "url": "/onlinebanking-Homepage/index.html"
+  },
+  {
+    "revision": "50dad2799ea7052b242a",
+    "url": "/onlinebanking-Homepage/static/css/main.356235f2.chunk.css"
+  },
+  {
+    "revision": "73475118d1f1ceec1e15",
+    "url": "/onlinebanking-Homepage/static/js/2.90a7aad3.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/onlinebanking-Homepage/static/js/2.90a7aad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50dad2799ea7052b242a",
+    "url": "/onlinebanking-Homepage/static/js/main.b61f6a79.chunk.js"
+  },
+  {
+    "revision": "ebf7dbee9f4c10cdd390",
+    "url": "/onlinebanking-Homepage/static/js/runtime-main.e53cb737.js"
+  }
+]);
